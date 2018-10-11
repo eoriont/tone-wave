1e5ffd798bdb4fbbd359e41d5eb5a4f183e014ee
mvcodeclub <sarah@mvcodeclub.com>
2018-10-10 19:30:48 -0700

    adding rainbow sine wave

--- sketch.js
+++ sketch.js
@@ -1,31 +1,54 @@
 var oscillator;
+var volume;
 var s;
+var vs;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(windowWidth, windowHeight);
+    colorMode(HSB, 100);
     oscillator = new Tone.Oscillator(440, "sine").toMaster().start();
-    oscillator.volume.value = -25;
-    s = new Slider(50, 50, 0, 100, 0, 250);
+
+    s = new Slider(50, 50, 0, 100, 0, 250, "Frequency");
+    vs = new Slider(50, 150, 0, 100, 0, 250, "Volume");
 }
 
 function draw() {
-    background(50)
+    background(0)
     let freq = map(s.val, 0, 100, 100, 5000);
     oscillator.frequency.value = freq
     s.tick();
     s.draw();
 
-    line(0, 0, cos(frameCount/freq)*500, sin(frameCount/freq)*500)
+    vs.tick();
+    vs.draw();
+
+    volume = vs.val;
+
+    oscillator.volume.value = volume;
+
+    wave(freq, volume);
+}
+
+function wave(freq, vol) {
+  let lastpoint = {x: 0, y:0};
+  for (let i = 0; i < width; i+=1) {
+    let x = i;
+    let y = sin(i * freq / 10000 + frameCount/10)*vol + 400;
+    stroke(map(y, 400-volume, 400+volume, 0, 100), 100, 100)
+    line(lastpoint.x, lastpoint.y, i, y);
+    lastpoint = {x, y};
+  }
 }
 
 class Slider {
-    constructor(x, y, min, max, val, size) {
+    constructor(x, y, min, max, val, size, text) {
         this.x = x;
         this.y = y;
         this.min = min;
         this.max = max;
         this.val = val || min;
         this.size = size || max;
+        this.text = text || "";
     }
 
     tick() {
@@ -40,5 +63,9 @@ class Slider {
         line(this.x, this.y, this.x+this.size, this.y);
         fill("red");
         ellipse(this.x+this.val*this.size/this.max, this.y, 50, 50);
+
+        textSize(32);
+        fill("white")
+        text(this.text, this.x + this.size/4, this.y)
     }
 }
